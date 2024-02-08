fa3b14c61f8c11e1ac382065c368a82eca0fee7b
lordfbm <fbmjogador@gmail.com>
2024-02-08 15:58:48 -0400

    add rota produtos

---
+++ pages/produtos/[id].js
@@ -0,0 +1,8 @@
+import { useRouter } from "next/router";
+function Produtos(){
+    const router = useRouter();
+    const id = router.query.id;
+    return <div>Id do produto: {id}</div>
+
+}
+export default Produtos;
