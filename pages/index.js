b257e19301857987d1285a109dbd53252deab847
lordfbm <fbmjogador@gmail.com>
2024-02-07 21:50:45 -0400

    componete de performance

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,15 @@
-import { useState } from "react";
-
+import Link from "next/link";
 function Home(){
     return (
     <div>
-        <h1>Home 4</h1><br></br>
-        <Contador/>
+        <h1>Home </h1><br></br>
+        <link href="/sobre">
+        <a>
+        Acessar página sobre
+        </a>
+        </link>
+        
     </div>
-    )
-}
-function Contador(){
-    const [contador, setContador] = useState(1);
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
-    return(
-        <div>{contador}<br></br><br></br>
-        <button onClick={adicionarContador}>Adcionar</button>
-        </div>
     )
 }
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+function Sobre(){
+    return (
+    <div><h1>Sobre </h1><br></br>
+    <link href="/">
+    <a>
+    Acessar página Home
+    </a>
+    </link>
+    </div>
+    )
+}
+export default Sobre
